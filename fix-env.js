93aa4f7fdc7316df4dd3736e5fad89d5b52c5b5d
kiki <happle.networks@gmail.com>
2025-11-30 14:47:43 +0900

    Delete fix-env.js

--- fix-env.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const envPath = path.join(__dirname, '.env');
-const content = 'GOOGLE_API_KEY=AIzaSyBC1Bp3UkvFPaM6imB69teuWcOm1obJhIc';
-
-fs.writeFileSync(envPath, content, 'utf8');
-console.log('Fixed .env file encoding.');
